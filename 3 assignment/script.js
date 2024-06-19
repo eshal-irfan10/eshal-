35062814c6e2a0d1bae4c7950da73f07d0f66acb
Eshal Irfan <155638363+eshal-irfan10@users.noreply.github.com>
2024-06-19 05:36:28 -0700

    Add files via upload

---
+++ 3 assignment/script.js
@@ -0,0 +1,21 @@
+// let e = ; let f = ; let a = ;
+// let cat_apple = hamza;
+// let eshu_09 = hamza; 
+// legal
+// let eshal_eshu =
+// let $eshal =
+// let eshuname =
+// let eshal17 =
+// let,message = 
+// illegal
+// let @eshal =
+// let \eshal = 
+// let !eshal =
+// let #eshal = 
+// let eshal eshu =
+// document.write("Rules for naming js variables")
+// document.write(" <br> <br> <br> Variables names can only contain, numbers, $, _ . for example: $my_1stVariable <br>") ;
+// 
+// document.write("Variable must begin with the $ or _ . for example: $name,_name or name <br>");
+// document.write("Variables names are case sensitive <br>" );
+// document.write("Variables names should not be JS keywords. <br>");
